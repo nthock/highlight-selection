aebbbe8ca90f0ae2a288e3c05e888f3e9c5640b2
Thiam Hock Ng <nthock@gmail.com>
2021-03-19 11:30:51 +0800

    Add active selection

--- pages/index.js
+++ pages/index.js
@@ -1,102 +1,98 @@
-import { useEffect, useState } from "react"
-import { Input } from "antd"
-import { CloseOutlined, SearchOutlined, UpOutlined, DownOutlined } from '@ant-design/icons';
+import { useReducer } from "react";
+import { Input } from "antd";
+import {
+  CloseOutlined,
+  SearchOutlined,
+  UpOutlined,
+  DownOutlined,
+} from "@ant-design/icons";
+
+import { contentReducer } from "../reducers/contentReducer";
 
 const contentList = [
-  { id: 1, text: "hello world" },
-  { id: 2, text: "another hello" }
-]
-
-const highlightText = (searchTerm, contentList) => {
-  if (searchTerm.length > 2) {
-    const updatedContentList = contentList.map(content => {
-      const updatedText = content.text.replace(new RegExp(searchTerm, "gi"), match => (
-        `<mark>${match}</mark>`
-      ))
-      return {
-        ...content,
-        text: updatedText
-      }
-    })
-    return updatedContentList
-  }
-  return contentList
-}
-
-const getSearchCount = (searchTerm, contentList) => {
-  if (searchTerm.length < 2) {
-    return 0
-  }
-  const regExp = new RegExp(searchTerm, "gi");
-
-  const count = contentList.reduce((acc, content) => {
-    return acc += (content.text.match(regExp) || []).length
-  }, 0)
-  console.log("count", count)
-  return count
-}
+  { id: 1, text: "hello world hello" },
+  { id: 2, text: "another hello" },
+  { id: 3, text: "another hello" },
+  { id: 4, text: "another hello" },
+  { id: 5, text: "another hello" },
+  { id: 6, text: "another hello" },
+  { id: 7, text: "another hello" },
+  { id: 8, text: "another hello" },
+];
 
 const Home = () => {
-  const [searchInput, setSearchInput] = useState("")
-  const [textContent, setTextContent] = useState(contentList)
+  const [state, dispatch] = useReducer(contentReducer, {
+    textContent: contentList,
+    searchCount: 0,
+    renderedContent: contentList,
+    currentSelection: 1,
+    searchInput: "",
+  });
 
   const onChange = (e) => {
-    setSearchInput(e.target.value)
-  }
+    dispatch({ type: "UPDATE_SEARCH_TERM", payload: { text: e.target.value } });
+  };
 
   const onUpdateText = (id, updatedText) => {
-    const updatedTextContent = textContent.map(content => {
-      if (content.id === id) {
-        const cleanText = updatedText.replace(/(<([^>]+)>)/ig, '')
-        return { ...content, text: cleanText }
-      }
-      return content
-    })
-    setTextContent(updatedTextContent)
-  }
-
-  const renderedContent = highlightText(searchInput, textContent)
-  const searchCount = getSearchCount(searchInput, textContent)
+    dispatch({ type: "UPDATE_CONTENT", payload: { id, updatedText } });
+  };
 
   return (
     <div>
       <div className="container mx-auto p-6">
         <div className="flex justify-center">
           <div className="w-full md:w-1/2">
-            <Input placeholder="search"
-              value={searchInput}
+            <Input
+              placeholder="search"
+              value={state.searchInput}
               onChange={onChange}
               prefix={<SearchOutlined />}
               suffix={
                 <>
-                  <p>1/{searchCount}</p>
-                  <UpOutlined className="mx-1" />
-                  <DownOutlined className="mx-1" />
-                  <CloseOutlined className="ml-2" onClick={() => setSearchInput("")} />
+                  <p>
+                    {state.currentSelection}/{state.searchCount}
+                  </p>
+                  <UpOutlined
+                    className="mx-1"
+                    onClick={() => dispatch({ type: "PREVIOUS_SELECTION" })}
+                  />
+                  <DownOutlined
+                    className="mx-1"
+                    onClick={() => dispatch({ type: "NEXT_SELECTION" })}
+                  />
+                  <CloseOutlined
+                    className="ml-2"
+                    onClick={() =>
+                      dispatch({
+                        type: "UPDATE_SEARCH_TERM",
+                        payload: { text: "" },
+                      })
+                    }
+                  />
                 </>
               }
             />
             <div className="mt-2">
-              {
-                renderedContent.map((content) => {
-                  return (
-                    <div className="my-4" key={`content-${content.id}`}>
-                      <span
-                        className="w-full outline-none"
-                        contentEditable dangerouslySetInnerHTML={{ __html: content.text }}
-                        onBlur={(e) => onUpdateText(content.id, e.target.innerHTML)}
-
-                      />
-                    </div>
-                  )
-                })
-              }
+              {state.renderedContent.map((content) => {
+                return (
+                  <div className="my-4" key={`content-${content.id}`}>
+                    <span
+                      className="w-full outline-none"
+                      contentEditable
+                      dangerouslySetInnerHTML={{ __html: content.text }}
+                      onBlur={(e) =>
+                        onUpdateText(content.id, e.target.innerHTML)
+                      }
+                    />
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
---
+++ reducers/contentReducer.js
@@ -0,0 +1,124 @@
+const updateTextContent = (textContent, { id, updatedText }) => {
+  return textContent.map((content) => {
+    if (content.id === id) {
+      const cleanText = updatedText.replace(/(<([^>]+)>)/gi, "");
+      return { ...content, text: cleanText };
+    }
+    return content;
+  });
+};
+
+const generateUpdatedText = (
+  content,
+  searchCount,
+  occurences,
+  currentSelection,
+  regExp
+) => {
+  if (currentSelection <= searchCount) {
+    return content.text.replace(
+      regExp,
+      (match) => `<mark class="selected">${match}</mark>`
+    );
+  }
+
+  if (currentSelection > searchCount + occurences.length) {
+    return content.text.replace(
+      regExp,
+      (match) => `<mark class="selected">${match}</mark>`
+    );
+  }
+
+  const selectedIndex = currentSelection - searchCount - 1;
+
+  return content.text.replace(regExp, (match, index) => {
+    if (index === occurences[selectedIndex].index) {
+      return `<mark class="active">${match}</mark>`;
+    }
+    return `<mark class="selected">${match}</mark>`;
+  });
+};
+
+const generateRenderedContentCount = ({
+  searchInput,
+  textContent,
+  currentSelection,
+}) => {
+  const regExp = new RegExp(searchInput, "gi");
+
+  return textContent.reduce(
+    (acc, content) => {
+      const occurences = [...content.text.matchAll(regExp)];
+      const updatedText = generateUpdatedText(
+        content,
+        acc.searchCount,
+        occurences,
+        currentSelection,
+        regExp
+      );
+
+      return {
+        ...acc,
+        searchCount: (acc.searchCount += occurences.length),
+        renderedContent: [
+          ...acc.renderedContent,
+          { ...content, text: updatedText },
+        ],
+      };
+    },
+    { searchCount: 0, renderedContent: [] }
+  );
+};
+
+const generateSearchData = (state) => {
+  const { searchInput } = state;
+
+  if (searchInput.length > 2) {
+    const searchData = generateRenderedContentCount(state);
+    return {
+      ...state,
+      ...searchData,
+    };
+  }
+
+  return { ...state, searchCount: 0, renderedContent: state.textContent };
+};
+
+const setPreviousSelection = ({ currentSelection }) => {
+  if (currentSelection === 1) {
+    return currentSelection;
+  }
+  return currentSelection - 1;
+};
+
+const setNextSelection = ({ currentSelection, searchCount }) => {
+  if (currentSelection >= searchCount) {
+    return currentSelection;
+  }
+  return currentSelection + 1;
+};
+
+export const contentReducer = (state, action) => {
+  switch (action.type) {
+    case "UPDATE_SEARCH_TERM":
+      return generateSearchData({ ...state, searchInput: action.payload.text });
+    case "UPDATE_CONTENT":
+      return generateSearchData({
+        ...state,
+        textContent: updateTextContent(state.textContent, action.payload),
+      });
+    case "PREVIOUS_SELECTION":
+      return generateSearchData({
+        ...state,
+        currentSelection: setPreviousSelection(state),
+      });
+    case "NEXT_SELECTION":
+      return generateSearchData({
+        ...state,
+        currentSelection: setNextSelection(state),
+      });
+    default:
+      console.log("action", action);
+      throw new Error();
+  }
+};
